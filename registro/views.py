3c971ec50c49125e6a52789ecac287c4cac29da0
Gerald Emmanuel Figueroa Gomez <gerald.figueroa@unah.hn>
2024-02-25 21:06:07 -0600

    Mejora(0.4.4) Configurando el envío de la cookie, httpOnly False

--- registro/views.py
+++ registro/views.py
@@ -86,7 +86,7 @@ class LoginView(APIView):
 
 
         response = Response()
-        response.set_cookie(key='jwt', value=token, httponly=True, samesite='None')
+        response.set_cookie(key='jwt', value=token, httponly=False)
         response.data = {
             'jwt': token,
             'persona': {
